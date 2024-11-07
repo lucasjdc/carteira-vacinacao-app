f9a4fb010d3406d18ca323a79b3130f3257302bb
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-11-07 19:32:25 -0300

    foi criado as funções
    Foi criada as funções para os botões para a tela de menu

---
+++ Menu.kt
@@ -0,0 +1,42 @@
+package br.senac.card
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class Menu : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_de_menu)
+        bt_perfil.setOnClickListener {
+            IrParaPerfil()
+        }
+    }
+
+    private fun IrParaPerfil() {
+
+        var perfil = Intent(this, Perfil::class.java)
+        startActivity(perfil)
+
+    }
+
+    private fun IrParaCarteiraDeVacinacao() {
+        var carteira = Intent(this, Carteira::class.java)
+        startActivity(carteira)
+    }
+
+    private fun IrParaAgendamentos() {
+        var agendamentos = Intent(this, Agendamentos::class.java)
+        startActivity(agendamentos)
+    }
+
+    private fun IrParaPosto() {
+        var posto = Intent(this, Posto::class.java)
+        startActivity(posto)
+    }
+
+    private fun IrParaSaudeGestante() {
+        var gestante = Intent(this, Gestante::class.java)
+        startActivity(gestante)
+    }
+}
